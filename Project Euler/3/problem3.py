f8cae931ea512b4815a5e5343c2fc29b69b78c35
tenspeed <tsmith86@gmail.com>
2013-11-19 09:42:33 -0800

    Solved Euler Problem # 3

--- Project Euler/3/problem3.py
+++ Project Euler/3/problem3.py
@@ -5,5 +5,49 @@
 #
 # What is the largest prime factor of the number 600851475143?
 
-# 600851475143 is a prime number so the only factors are itself and 1, right?
-# Is this a trick question?
+def get_primes(num, prime_list):
+	for i in range(500):
+		if is_prime(num, prime_list):
+			prime_list.append(num)
+		else:
+			num += 1
+	return prime_list
+
+def is_prime(num, prime_list):
+	for i in range(len(prime_list)):
+		if num % prime_list[i] == 0:
+			break
+		else:
+			if i == (len(prime_list) - 1):
+				return True
+			else:
+				pass
+	return False
+
+p_factors = []
+primes = [2, 3, 5, 7, 9]
+quotient = 600851475143
+p_found = False
+go = True
+
+for i in range(15):
+	primes = get_primes((primes[-1] + 1), primes)
+
+while go:
+	for element in primes:
+		if (quotient % element == 0) and (quotient != element):
+			quotient /= element
+			p_found = True
+			p_factors.append(element)
+			if is_prime(quotient, primes):
+				p_factors.append(quotient)
+				go = False
+		else:
+			pass
+
+	if (p_found == False):
+		primes = get_primes((primes[-1] + 1), primes)
+	else:
+		p_found == False
+
+print p_factors
