9725cedbcf4f52b1fe0ce01beec38edc3ef6343c
tenspeed <tsmith86@gmail.com>
2013-11-21 15:56:53 -0800

    Solved Euler Problem #11

--- Project Euler/11/problem11.py
+++ Project Euler/11/problem11.py
@@ -20,48 +20,37 @@ for i in range(len(num_list)):
 
 for m in range(len(num_list_2D)):
 	for n in range(len(num_list_2D)):	
-		
-		print "m: %d" % m
-		print "n: %d" % n
-
 		if (m >= len(num_list_2D) - 3):
 			pass
 		else:
 			if (n < 3):
 				product = num_list_2D[m][n]*num_list_2D[m+1][n+1]*num_list_2D[m+2][n+2]*num_list_2D[m+3][n+3]
-				print "product dr: %d" % product
 				if product > largest_product:
 					largest_product = product
 			elif (n > (len(num_list_2D) - 4)):
-				product = num_list_2D[m][n]*num_list_2D[m-1][n-1]*num_list_2D[m-2][n-2]*num_list_2D[m-3][n-3]
-				print "product dl: %d" % product
+				product = num_list_2D[m][n]*num_list_2D[m+1][n-1]*num_list_2D[m+2][n-2]*num_list_2D[m+3][n-3]
 				if product > largest_product:
 					largest_product = product
 			else:
 				product = num_list_2D[m][n]*num_list_2D[m+1][n+1]*num_list_2D[m+2][n+2]*num_list_2D[m+3][n+3]
-				print "product drl: %d" % product
 				if product > largest_product:
 					largest_product = product
 
-				product = num_list_2D[m][n]*num_list_2D[m-1][n-1]*num_list_2D[m-2][n-2]*num_list_2D[m-3][n-3]
-				print "product dlr: %d" % product
+				product = num_list_2D[m][n]*num_list_2D[m+1][n-1]*num_list_2D[m+2][n-2]*num_list_2D[m+3][n-3]
 				if product > largest_product:
 					largest_product = product
 
 		if n <= (len(num_list_2D) - 4):
 			product = num_list_2D[m][n]*num_list_2D[m][n+1]*num_list_2D[m][n+2]*num_list_2D[m][n+3]
-			print "product r: %d" % product
 			if product > largest_product:
 				largest_product = product
 		else:
 			pass
 		if m <= (len(num_list_2D) - 4):
 			product = num_list_2D[m][n]*num_list_2D[m+1][n]*num_list_2D[m+2][n]*num_list_2D[m+3][n]
-			print "product c: %d" % product
 			if product > largest_product:
 				largest_product = product
 		else:
 			pass
-
-		raw_input("> ")
+			
 print "largest product: %d" % largest_product
