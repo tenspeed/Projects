c8441ec03d506a3950cfec01fc52918f6bc43307
tenspeed <tsmith86@gmail.com>
2013-12-02 20:46:13 -0800

    Started tearing apart bmdb
    working on adding sqlalchemy stuff :)

--- Bobs_Movie_Database/bmdb/bmdb.py
+++ Bobs_Movie_Database/bmdb/bmdb.py
@@ -1,4 +1,4 @@
-# tmdb.py version 2.0
+# bmdb.py version 3.0
 # Written by Todd Smith
 # 10/15/2013
 # Licence:
@@ -8,13 +8,13 @@
 # This is the Database class. The Database class handles any task relating to the movie database including,
 # opening the file where the movie information is stored, searching for movies based on search criteria,
 # and adding new movies to the database.
+from sqlalchemy import *
+
 class Database(object):
 
 	# Upon instantiation, a permanent database property is created by calling the load_db() method.
 	def __init__(self):
-
-		self.the_database = self.load_db()
-
+		pass
 	# The search() method accepts a query in the form of a string as an argument and
 	# searches through the movie database looking for other strings that match or are
 	# a partial match to the query. If a match is found, search() returns a formated,
@@ -66,26 +66,63 @@ class Database(object):
 
 	# The load_db() method initializes the_database. It's called when the database object
 	# is first created and whenever new movie info is added by the user.
-	def load_db(self):
+	def create_db(self):
 
-		# Open the text file where the movie information is saved and read it into the variable 'data'
-		# as a string.
-		with open("bmdb.txt", "a+") as f:
-			data = f.read()
+		engine = create_engine("sqlite:///bmdb.db")
 
-		# Remove the last ';' from the end of the string.
-		data = data[:-1]
+		metadata = MetaData()
 
-		# The six categories of information in the text file are 'Title', 'Genre', 'Director',
-		# 'Year', 'Format', and 'Actors', in that order. Each entry in the text file is separated by a
-		# semi-colon. Here, we take the raw information from the 'data' string and create a list
-		# containing the information for each category by looking for the semi-colons.
-		movies = data.split(';')
+		movie = Table('movie', metadata,
+					  Column('id', Integer, primary_key=True),
+					  Column('title', String),
+					  Column('year', Integer)
+					 )
 
-		# The data taken from the save file is organized into a 2D list by calling list_builder().
-		moviedb = self.list_builder(movies, 6)
-		return moviedb
+		director = Table('director', metadata,
+						 Column('id', Integer, primary_key=True),
+						 Column('first_name', String),
+						 Column('last_name', String),
+						 Column('fullname', String)
+						)
 
+		actor = Table('actor', metadata,
+					  Column('id', Integer, primary_key=True),
+					  Column('first_name', String),
+					  Column('last_name', String),
+					  Column('fullname', String)
+					 )
+
+		format = Table('format', metadata,
+					   Column('id', Integer, primary_key=True),
+					   Column('format_type', String)
+					  )
+
+		genre = Table('genre', metadata,
+					  Column('id', Integer, primary_key=True),
+					  Column('genre_name', String)
+					 )
+
+		movie_director = Table('movie_director', metadata,
+							   Column('movie_id', Integer),
+							   Column('director_id', Integer)
+							  )
+
+		movie_actor = Table('movie_actor', metadata,
+							Column('movie_id', Integer),
+							Column('actor_id', Integer)
+						   )
+
+		movie_format = Table('movie_format', metadata,
+							 Column('movie_id', Integer),
+							 Column('format_id', Integer)
+							)
+
+		movie_genre = Table('movie_genre', metadata,
+							Column('movie_id', Integer),
+							Column('genre_id', Integer)
+						   )
+
+		metadata.create_all(engine)
 	# The list_formatter() method takes an unformatted 2D list and returns a formatted 2D list
 	# with the first letter of every word capitolized. Unformatted lists are for searching while
 	# formatted lists are for printing and displaying.
@@ -124,24 +161,11 @@ class Database(object):
 		return list_2D
 
 	def save_db(self, list_2D):
-		with open("bmdb.txt", "w") as f:
-			for i in range(len(list_2D)):
-				for j in range(6):
-					entry = list_2D[i][j]
-					entry += ";"
-					f.write(entry)
-		self.load_db()
+		pass
 
 	def delete_entries(self, del_movie_dict):
-		count = 0
-		indices = del_movie_dict.values()
-		indices.sort()
-		for i in range(len(indices)):
-			indices[i] = int(indices[i])
-			indices[i] -= count
-			deleted_movie = self.the_database.pop(indices[i])
-			count += 1
-		self.save_db(self.the_database)
+		pass
 
 #-----------------------------------------------------------------------------------------------------------
-#movies = Database()
+movies = Database()
+movies.create_db()
